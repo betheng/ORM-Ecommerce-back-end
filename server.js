8b1cb8c5ed2c8d07b65514db74cfefcb16bcb190
betheng <131614496+betheng@users.noreply.github.com>
2023-09-03 16:58:20 -0400

    add comments to server.js starter code for better understanding

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+// Import modules and routes
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 
+// Initialize express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware to pars json and encode
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Start server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
