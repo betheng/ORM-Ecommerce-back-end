c05d73c16714a3646b32286e76de01ac27510399
betheng <131614496+betheng@users.noreply.github.com>
2023-09-03 15:41:36 -0400

    add polymorph info for category extending model

--- models/Category.js
+++ models/Category.js
@@ -4,11 +4,22 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// Set up fields and rules for the Category model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
+// Config options
     sequelize,
     timestamps: false,
     freezeTableName: true,
