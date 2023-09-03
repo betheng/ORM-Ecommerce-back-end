224e3fe4512b94b5cb38f74bffbfb01a5df4ab4f
betheng <131614496+betheng@users.noreply.github.com>
2023-09-03 16:02:16 -0400

    add polymorph info for product, producttag, tag extending model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,14 +1,36 @@
+// Import sequelize modules, and db connection
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
+// Define the fields and rules for the ProductTag model
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
+// Config options
     sequelize,
     timestamps: false,
     freezeTableName: true,
--- models/Tag.js
+++ models/Tag.js
@@ -1,14 +1,25 @@
+// Import sequelize module and db config connection
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
+// Define the fields and rules for the Tag model
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
+// Config options
     sequelize,
     timestamps: false,
     freezeTableName: true,
